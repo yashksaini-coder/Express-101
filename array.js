b255222b59c29ae64f1ee3635fd2231975b55006
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-20 17:26:34 +0530

    feat: Add users array
    The `array.js` file is added, which exports an array of user objects. Each user object contains an `id`, `name`, and `kidney` property. This array will be used for further development.

---
+++ array.js
@@ -0,0 +1,7 @@
+const users = [
+    { id: 1, name: 'Yash', kidney: 'good' },
+    { id: 2, name: 'Deepanshu', kidney: 'bad' },
+    { id: 3, name: 'Rahul', kidney: 'good' }
+];
+
+module.exports = users;
