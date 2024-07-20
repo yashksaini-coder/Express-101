be0d4d98b018b39117ecc1c91b2f9e4a8cca4cfa
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-20 14:08:47 +0530

    Fix: Add parameters for name & number

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express();
 //Adding a route
 
 app.get("/", function(req, res){
-    res.send('Hello World');
+    // Add a paramerter to the send method
+    const n = req.query.n;
+    const name = req.query.name;
+    console.log(n);
+    console.log(name);
+    res.send('Hi the user is ' + name + ' and the answer is ' + n);
 })
 
 app.listen(3000);
