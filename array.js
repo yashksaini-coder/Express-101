557793ef5bc667ecd705cc89e75a5a90a589fda3
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-20 18:39:23 +0530

    chore: Add fetch function for array objects

--- array.js
+++ array.js
@@ -1,7 +1,25 @@
+const express = require('express');
+const app = express(); 
+
 const users = [
     { id: 1, name: 'Yash', kidney: 'good' },
     { id: 2, name: 'Deepanshu', kidney: 'bad' },
     { id: 3, name: 'Rahul', kidney: 'good' }
 ];
 
-module.exports = users;
+
+app.get('/',(req,res) => {
+    res.send("Server is working fine!");
+});
+
+app.get('/user', (req, res) => {
+    const id = parseInt(req.query.id);
+    const user = users.find(u => u.id === id);
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).send('User not found');
+    }
+});
+
+app.listen(3000);
