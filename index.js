136c8e9119d4a90974e660987b8da3191f1213dc
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-20 14:22:28 +0530

    chore: Add comments explaining request methods and status codes

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ const express = require('express');
 
 const app = express();
 
-//Adding a route
+//Request methods ==> GET, POST, PUT, DELETE
+// Get ==> Fetch data from the server
+// POST ==> Send data to the server
+// PUT ==> Update data on the server
+// DELETE ==> Delete data on the server
+
+// Status Codes ==> 200, 404, 500, 403, 401, 400
+// 200 ==> OK
+// 404 ==> Not Found
+// 500 ==> Internal Server Error
+// 403 ==> Forbidden
+// 401 ==> Unauthorized
+// 400 ==> Bad Request
 
 // req, res ==> request and response objects
 app.get("/", function(req, res){
-    // Add a paramerter to the send method
+    console.log("hi there!");
 })
 
 app.listen(3000);
