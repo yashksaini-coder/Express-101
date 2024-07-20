f92cb86866513af5e373dccf1d235a341a67bcf9
Yash Kumar Saini <115717039+yashksaini-coder@users.noreply.github.com>
2024-07-20 14:12:52 +0530

    Feat: Making a working hospital request & response control

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 //Creating an http server using Express library
 // node default library ==> NO
-
+// Express need to be installed locally using npm ==> npm install express
 const express = require('express');
 
 const app = express();
 
 //Adding a route
 
+// req, res ==> request and response objects
 app.get("/", function(req, res){
     // Add a paramerter to the send method
-    const n = req.query.n;
-    const name = req.query.name;
-    console.log(n);
-    console.log(name);
-    res.send('Hi the user is ' + name + ' and the answer is ' + n);
 })
 
 app.listen(3000);
